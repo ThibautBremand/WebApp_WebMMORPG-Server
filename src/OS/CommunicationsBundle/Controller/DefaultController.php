666b1e638d3231cde527352dd28734a89bd125b1
ThibautBremand <thibaut.bremand@orange.fr>
2015-06-14 23:18:42 +0200

    server now launchable through a browser with /comm/launch route

--- src/OS/CommunicationsBundle/Controller/DefaultController.php
+++ src/OS/CommunicationsBundle/Controller/DefaultController.php
@@ -5,6 +5,9 @@ namespace OS\CommunicationsBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Response;
+
+use Ratchet\Server\IoServer;
 
 class DefaultController extends Controller
 {
@@ -15,5 +18,15 @@ class DefaultController extends Controller
     public function indexAction($name)
     {
         return array('name' => $name);
+    }
+
+    /**
+     * @Route("/launch")
+     */
+    public function launchAction() {
+        $chat = $this->get('chat');
+        $server = IoServer::factory($chat, 8080);
+        $server->run();
+        return new Response('');
     }
 }
